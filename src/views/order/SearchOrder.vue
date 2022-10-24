a002bcf9ad8b9ff3a739b253ab3aedea7ca2a074
kyle <kyle@addtune.net>
2022-10-24 14:13:54 +0900

    data load test

--- src/views/order/SearchOrder.vue
+++ src/views/order/SearchOrder.vue
@@ -16,6 +16,27 @@
 <script>
 import BookList from "@/components/search/BookList";
 import SearchBasicGroup from "@/components/form/SearchBasicGroup";
+import { db } from "@/utils/db";
+import {
+  collection,
+  doc,
+  query,
+  orderBy,
+  limit,
+  getDocs,
+  startAt,
+  getDoc,
+  addDoc,
+  setDoc,
+  updateDoc,
+  arrayUnion,
+  arrayRemove,
+  where,
+  collectionGroup,
+  startAfter,
+  deleteDoc,
+  endBefore,
+} from "firebase/firestore";
 export default {
   components: { SearchBasicGroup, BookList },
   data() {
@@ -26,7 +47,30 @@ export default {
         { item: "저자", value: "03" },
         { item: "출판사", value: "04" },
       ],
+      limit: 2,
+      booksAll: [],
+      books: [],
+      page: 0,
     };
+  },
+  mounted() {
+    this.callDB();
+  },
+  methods: {
+    async callDB() {
+      try {
+        const first = query(collection(db, "booksData"), orderBy("subject"), limit(this.limit));
+        const documentSnapshots = await getDocs(first);
+        console.log(documentSnapshots);
+        this.lastVisible = documentSnapshots.docs[1];
+        documentSnapshots.forEach(doc => {
+          console.log(doc);
+          //his.booksAll.push({ id: doc.id, data: doc.data() });
+        });
+      } catch (e) {
+        console.error("Error adding document: ", e);
+      }
+    },
   },
 };
 </script>
