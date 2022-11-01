191540bf2ce1ce954cc66e7b8e2449ed999238d8
kyle <kyle@addtune.net>
2022-11-01 11:49:44 +0900

    중복함수 변경 & 회신후 수량 변경

--- src/utils/arrMerge.js
+++ src/utils/arrMerge.js
@@ -1,22 +1,27 @@
 export default function arrMerge(arr) {
   const resultArr = [];
+  let flag = false;
+  let cnt = 0;
   for (let i = 0; i < arr.length; i++) {
-    let idx = getKeyIndex(resultArr, arr[i]);
-    if (idx > -1) {
-      //같은종 더하기
-      resultArr[idx].count += Number(arr[i].count);
-    } else {
+    cnt = 0;
+    flag = false;
+    for (let value in resultArr) {
+      // 중복 체크
+      if (resultArr[value].publisher === arr[i].publisher && resultArr[value].timestamp === arr[i].timestamp) {
+        flag = true;
+      }
+      if (flag) {
+        ++cnt;
+        if (resultArr[value].shop_order_status === 0) {
+          resultArr[value].count += Number(arr[i].count);
+        } else {
+          resultArr[value].reply_count += Number(arr[i].reply_count);
+        }
+      }
+    }
+    if (cnt === 0) {
       resultArr.push(arr[i]);
     }
   }
   return resultArr;
-}
-//중복제거
-function getKeyIndex(arr, obj) {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i].publisher === obj.publisher && arr[i].timestamp === obj.timestamp) {
-      return i;
-    }
-  }
-  return -1;
 }
