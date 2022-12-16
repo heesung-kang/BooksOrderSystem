65d6292a7f2007cba5cd8d5f1712818821abe8fa
kyle <kyle@addtune.net>
2022-12-16 11:00:53 +0900

    호스팅 경로 변경

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
 
 //const TerserPlugin = require('terser-webpack-plugin');
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/BooksOrderSystem/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/store/" : "/",
   lintOnSave: false,
   transpileDependencies: ["vuetify"],
   productionSourceMap: false,
